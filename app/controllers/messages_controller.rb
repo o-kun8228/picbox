9c08403d4c995c3bd69fb87c9e477ff800bae034
o-kun8228 <92617996+o-kun8228@users.noreply.github.com>
2021-12-10 12:23:43 +0900

    新規投稿画像から流れるように

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
     @message = Message.new
     @room = Room.find(params[:room_id])
     @messages = @room.messages.includes(:user)
+    @messages = Message.includes(:user).order("created_at DESC")
   end
 
 
