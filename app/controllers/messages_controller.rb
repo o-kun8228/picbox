af1ae13b3db7052451fb80dbd5b765323957057a
o-kun8228 <92617996+o-kun8228@users.noreply.github.com>
2021-12-12 19:39:35 +0900

    ルームが変わらなかったコントローラーの修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
-    @messages = @room.messages.includes(:user)
-    @messages = Message.includes(:user).order("created_at DESC")
+    @messages = @room.messages.includes(:user).order("created_at DESC")
   end
 
 
